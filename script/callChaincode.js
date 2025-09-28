365fd81acf876411f31c264337e310e68fccae65
Ankit Chauhan <65777088+Saurav036@users.noreply.github.com>
2025-09-28 21:21:01 +0100

    'ankit2'

--- script/callChaincode.js
+++ script/callChaincode.js
@@ -28,46 +28,46 @@ async function main() {
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the user.
-        // const userIdentity = await wallet.get('insuranceAgent01-Rama');
-        // if (userIdentity) {
-        //     console.log('An identity for the user "insuranceAgent01-Rama" already exists in the wallet');
-        //     return;
-        // }
+        const userIdentity = await wallet.get('insuranceAgent01-Rama');
+        if (userIdentity) {
+            console.log('An identity for the user "insuranceAgent01-Rama" already exists in the wallet');
+            return;
+        }
 
-        // // Check to see if we've already enrolled the insuranceAdmin user.
-        // const adminIdentity = await wallet.get('insuranceAdmin');
-        // if (!adminIdentity) {
-        //     console.log('An identity for the insuranceAdmin user "insuranceAdmin" does not exist in the wallet');
-        //     console.log('Run the enrollAdmin.js application before retrying');
-        //     return;
-        // }
+        // Check to see if we've already enrolled the insuranceAdmin user.
+        const adminIdentity = await wallet.get('insuranceAdmin');
+        if (!adminIdentity) {
+            console.log('An identity for the insuranceAdmin user "insuranceAdmin" does not exist in the wallet');
+            console.log('Run the enrollAdmin.js application before retrying');
+            return;
+        }
 
-        // // build a user object for authenticating with the CA
-        // const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
-        // const adminUser = await provider.getUserContext(adminIdentity, 'insuranceAdmin');
+        // build a user object for authenticating with the CA
+        const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
+        const adminUser = await provider.getUserContext(adminIdentity, 'insuranceAdmin');
 
-        // // Register the user, enroll the user, and import the new identity into the wallet.
-        // const secret = await ca.register({
-        //     affiliation: 'org2.department1',
-        //     enrollmentID: 'insuranceAgent01-Rama',
-        //     role: 'client',
-        //     attrs: [{ name: 'role', value: 'Doctor', ecert: true },{ name: 'uuid', value: 'insuranceAgent01-Rama', ecert: true }],
-        // }, adminUser);
-        // const enrollment = await ca.enroll({
-        //     enrollmentID: 'insuranceAgent01-Rama',
-        //     enrollmentSecret: secret,
-        //     attr_reqs: [{ name: "role", optional: false },{ name: "uuid", optional: false }]
-        // });
-        // const x509Identity = {
-        //     credentials: {
-        //         certificate: enrollment.certificate,
-        //         privateKey: enrollment.key.toBytes(),
-        //     },
-        //     mspId: 'Org2MSP',
-        //     type: 'X.509',
-        // };
-        // await wallet.put('insuranceAgent01-Rama', x509Identity);
-        // console.log('Successfully registered and enrolled insuranceAdmin user "insuranceAgent01-Rama" and imported it into the wallet');
+        // Register the user, enroll the user, and import the new identity into the wallet.
+        const secret = await ca.register({
+            affiliation: 'org2.department1',
+            enrollmentID: 'insuranceAgent01-Rama',
+            role: 'client',
+            attrs: [{ name: 'role', value: 'Doctor', ecert: true },{ name: 'uuid', value: 'insuranceAgent01-Rama', ecert: true }],
+        }, adminUser);
+        const enrollment = await ca.enroll({
+            enrollmentID: 'insuranceAgent01-Rama',
+            enrollmentSecret: secret,
+            attr_reqs: [{ name: "role", optional: false },{ name: "uuid", optional: false }]
+        });
+        const x509Identity = {
+            credentials: {
+                certificate: enrollment.certificate,
+                privateKey: enrollment.key.toBytes(),
+            },
+            mspId: 'Org2MSP',
+            type: 'X.509',
+        };
+        await wallet.put('insuranceAgent01-Rama', x509Identity);
+        console.log('Successfully registered and enrolled insuranceAdmin user "insuranceAgent01-Rama" and imported it into the wallet');
 
         // -----------------------Create Wallet with default balance on ledger------------------ 
                 // Create a new gateway for connecting to our peer node.
