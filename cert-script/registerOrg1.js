5b07c8835faf42f119962b635c65922c29b85ebe
Ankit Chauhan <65777088+Saurav036@users.noreply.github.com>
2025-09-28 20:09:00 +0100

    'ankit2'

--- cert-script/registerOrg1.js
+++ cert-script/registerOrg1.js
@@ -14,11 +14,11 @@ const path = require('path');
 async function main() {
     try {
         // load the network configuration
-        const ccpPath = path.resolve(__dirname, '..', 'fabric-samples', 'test-network', 'organizations', 'peerOrganizations', 'org2.example.com', 'connection-org2.json');
+        const ccpPath = path.resolve(__dirname, '../..', 'fabric-samples', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
         const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
 
         // Create a new CA client for interacting with the CA.
-        const caInfo = ccp.certificateAuthorities['ca.org2.example.com'];
+        const caInfo = ccp.certificateAuthorities['ca.org1.example.com'];
         const caTLSCACerts = caInfo.tlsCACerts.pem;
         const ca = new FabricCAServices(caInfo.url, { trustedRoots: caTLSCACerts, verify: false }, caInfo.caName);
 
@@ -28,9 +28,9 @@ async function main() {
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the admin user.
-        const identity = await wallet.get('insuranceAdmin');
+        const identity = await wallet.get('hospitalAdmin');
         if (identity) {
-            console.log('An identity for the admin user "insuranceAdmin" already exists in the wallet');
+            console.log('An identity for the admin user "admin" already exists in the wallet');
             return;
         }
 
@@ -41,14 +41,14 @@ async function main() {
                 certificate: enrollment.certificate,
                 privateKey: enrollment.key.toBytes(),
             },
-            mspId: 'Org2MSP',
+            mspId: 'Org1MSP',
             type: 'X.509',
         };
-        await wallet.put('insuranceAdmin', x509Identity);
-        console.log('Successfully enrolled admin user "insuranceAdmin" and imported it into the wallet');
+        await wallet.put('hospitalAdmin', x509Identity);
+        console.log('Successfully enrolled admin user "admin1" and imported it into the wallet');
 
     } catch (error) {
-        console.error(`Failed to enroll admin user "insuranceAdmin": ${error}`);
+        console.error(`Failed to enroll admin user "admin1": ${error}`);
         process.exit(1);
     }
 }
